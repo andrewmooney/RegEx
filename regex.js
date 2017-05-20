048d3b2f58a13e4428bf11707f186849ef2eae26
Andrew Mooney <amooney@me.com>
2017-05-20 18:02:08 +1000

    Added regular expression for password strength.

--- regex.js
+++ regex.js
@@ -4,3 +4,8 @@ var test = urlRegex.exec("http:www.example.com/#1234"); // Returns array
 var test2 = urlRegex.test("https:www.example.com/#1234"); // Returns boolean
 console.log(test);
 console.log(test2);
+
+// Regular expression to test if a password includes at least one capital letter, lowercase letter, number and symbol.
+let passwordRegex = new RegExp("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#£$%^&*-_=+\:;])(?=.{8,})");
+const test3 = passwordRegex.test("Ab1$"); // Returns true
+const test4 = passwordRegex.test("Ab1"); // Returns false
